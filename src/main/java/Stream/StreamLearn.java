dccac06d0db7dd5a8f7bf94e2a18877f1278ae63
Prathap2307 <prathapa123@gmail.com>
2021-07-22 18:20:06 +0530

    july 22

--- src/main/java/Stream/StreamLearn.java
+++ src/main/java/Stream/StreamLearn.java
@@ -1,6 +1,7 @@
 package Stream;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -32,9 +33,7 @@ class Movie {
 		this.price = price;
 	}
 
-
 }
-
 
 class Product {
 	int id;
@@ -73,46 +72,67 @@ public class StreamLearn {
 		productsList.add(new Product(3, "Lenevo Laptop", 28000f));
 		productsList.add(new Product(4, "Sony Laptop", 28000f));
 		productsList.add(new Product(5, "Apple Laptop", 90000f));
-		
+
 		List<Float> productPriceList2 = productsList.stream().filter(p -> p.price > 30000)// filtering data
 				.map(p -> p.price * 2) // fetching price
 				.collect(Collectors.toList()); // collecting as list
-		
-		
 
 		List<Product> filterdObj = productsList.stream().filter(p -> p.price > 30000)// filtering data
 				.collect(Collectors.toList()); // collecting as list
 
-		//int hasCodePrice = productsList.stream().filter(p -> p.price > 30000).map(p -> p.price).hashCode();
-		//System.out.println("hasCodePrice -->" + hasCodePrice);
-		//System.out.println(productPriceList2);
+		// int hasCodePrice = productsList.stream().filter(p -> p.price > 30000).map(p
+		// -> p.price).hashCode();
+		// System.out.println("hasCodePrice -->" + hasCodePrice);
+		// System.out.println(productPriceList2);
 
 		// System.out.println("filterdObj -->" + filterdObj[0].get);
 
 		for (Product obj : filterdObj) {
 			System.out.println("obj -->" + obj.getName());
 		}
-		
+
 		List<Product> singleObj = productsList.stream().filter(p -> p.price > 30000)// filtering data
 				.collect(Collectors.toList()); // collecting as list
-		
-		
-		
-		
+
 		List<Movie> movieList = new ArrayList<Movie>();
-	
+
 		movieList.add(new Movie(1, "inception", 10));
 		movieList.add(new Movie(1, "iron man", 15));
 		movieList.add(new Movie(1, "thor", 20));
-		
-		
-		Predicate<Movie> noGreaterThan10 =  x -> x.getPrice() > 10;
-		
+
+		Predicate<Movie> noGreaterThan10 = x -> x.getPrice() > 10;
+
 		List<Movie> movieFilteredList = movieList.stream().filter(noGreaterThan10)// filtering data
 				.collect(Collectors.toList()); // collecting as list
-		
-		for( Movie movie : movieFilteredList) {
-				System.out.println(movie.getName()+ " ----- " + movie.getPrice());
+
+		for (Movie movie : movieFilteredList) {
+			System.out.println("before -->" + movie.getName() + " ----- " + movie.getPrice());
 		}
+
+		//for (Movie movie : movieFilteredList) {
+			/*
+			 * if(movie.getName().equalsIgnoreCase("thor")) {
+			 * movieFilteredList.remove(movie); }
+			 */
+			
+			/*
+			 * if(movie.getName() == "thor") { movieFilteredList.remove(movie); }
+			 */
+		//}
+		
+		
+		for ( Iterator<Movie> iterator = movieFilteredList.iterator(); iterator.hasNext(); ) {
+			Movie movie = iterator.next();
+		    if (movie.getName() == "thor") {
+		        iterator.remove();
+		    }
+		}
+		
+		
+		for (Movie movie : movieFilteredList) {
+			System.out.println("after -->" + movie.getName() + " ----- " + movie.getPrice());
+		}
+		
+		
 	}
 }
